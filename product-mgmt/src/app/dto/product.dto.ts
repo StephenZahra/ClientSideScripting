79a39a66e823e9555f828756d3efa1229fe5f3ab
stephen <stephenzahra101@gmail.com>
2021-11-08 14:19:20 +0100

    Continued Class Example

---
+++ product-mgmt/src/app/dto/product.dto.ts
@@ -0,0 +1,12 @@
+export class Product{
+    private id: number;
+    private name: string;
+    private code: string;
+    private releaseDate: Date;
+    private description: string;
+    private price: number;
+    private starRating: number;
+    private imageURL: string ;
+
+    
+}
